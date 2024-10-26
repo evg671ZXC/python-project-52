724c07471dcf4c801f38695c244fb574e283a9cc
evg671ZXC <evgmine671@gmail.com>
2024-10-26 21:01:57 +0600

    fix labels delete view

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.contrib.messages.views import SuccessMessageMixin
@@ -32,3 +34,10 @@ class LabelDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     success_url = reverse_lazy('labels')
     success_message = 'Label successfully deleted'
     error_message = 'Cannot delete label because it is in use'
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.label.all().exists():
+            messages.error(request, self.error_message)
+            return redirect(self.success_url)
+        return super().post(request, *args, **kwargs)
