5f3ad61490e7cb902a372c9cef88173715c70d6a
evg671ZXC <evgmine671@gmail.com>
2024-10-26 19:44:01 +0600

    fix connected rollbar

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 
+# def index(request):
+#     return render(request, 'index.html')
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
 def index(request):
-    return render(request, 'index.html')
+    a = None
+    a.hello() # Creating an error with an invalid line of code
+    return HttpResponse("Hello, world. You're at the pollapp index.")
