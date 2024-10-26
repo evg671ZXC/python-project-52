1f387a16e600046f7f55006db07319f74ddffafe
evg671ZXC <evgmine671@gmail.com>
2024-10-26 20:09:19 +0600

    check rollbar

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
 
 
-# def index(request):
-#     return render(request, 'index.html')
-
-from django.shortcuts import render
-from django.http import HttpResponse
-
 def index(request):
-    a = None
-    a.hello() # Creating an error with an invalid line of code
-    return HttpResponse("Hello, world. You're at the pollapp index.")
+    return render(request, 'index.html')
