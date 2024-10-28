a4b0e6b725989f71f31e9f2a6f50d6fa94e64c2e
evg671ZXC <evgmine671@gmail.com>
2024-10-28 23:44:44 +0600

    fix(labels, statuses): correct fields name

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Label(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name='name')
+    name = models.CharField(max_length=100, unique=True, verbose_name=_('Name'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
